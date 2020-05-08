108bfe47e5817df98a4a93e6464a72bbeaadac21
Sébastien MURGEY <sebastien.murgey@gmail.com>
2020-05-08 15:40:38 +0200

    Initial version of Project Ramuh deployment

---
+++ overlays/us/config.template.js
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+window.RamuhEnv = {
+  RAMUH_GRID_OVERLAY_PROVIDER_URL: "http://<PUBLIC-IP>/api/grid-overlay-provider/overlay",
+  INITIAL_MAP_CENTER_LAT: 38.2,
+  INITIAL_MAP_CENTER_LNG: -90,
+  INITIAL_MAP_ZOOM: 4,
+}
+  
